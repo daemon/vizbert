Fix precision calculation

vizbert/data/metric.py
@@ -59,16 +59,23 @@ class RecallMetric(Metric, name='recall'):
         return (((scores > 0) & (gold > 0)).float().sum(-1) / gold.float().sum(-1)).mean()
 
 
+class F1Metric(Metric, name='f1'):
+
+    def _evaluate(self, scores: torch.Tensor, gold: torch.Tensor):
+        recall = (((scores > 0) & (gold > 0)).float().sum(-1) / gold.float().sum(-1)).mean()
+        sum_scores = scores.float().sum(-1)
+        sum_scores[sum_scores == 0] = 1
+        prec = (((scores > 0) & (gold > 0)).float().sum(-1) / sum_scores).mean()
+        return 2 * prec * recall / (prec + recall)
+
+
 class PrecisionMetric(Metric, name='precision'):
 
     def _evaluate(self, scores: torch.Tensor, gold: torch.Tensor):
-        num_nnz_scores = (scores > 0).float().sum(-1)
-        if num_nnz_scores.sum().item() == 0:
-            return torch.zeros(1).to(scores.device)
-        scores = scores[num_nnz_scores > 0]
-        gold = gold[num_nnz_scores > 0]
-        prec = ((scores > 0) & (gold > 0)).float().sum(-1) / num_nnz_scores[num_nnz_scores > 0].sum(-1)
-        return prec.mean()
+        sum_scores = scores.float().sum(-1)
+        sum_scores[sum_scores == 0] = 1
+        prec = (((scores > 0) & (gold > 0)).float().sum(-1) / sum_scores).mean()
+        return prec
 
 
 class PearsonrMetric(Metric, name='pearsonr'):

vizbert/model/loss.py
@@ -53,7 +53,7 @@ class ReconstructionLoss(nn.Module):
 
     def forward(self, scores, gold_scores, *args):
         if self.regression:
-            return self.criterion(scores, gold_scores)
+            return self.criterion(F.sigmoid(scores), F.sigmoid(gold_scores))
         else:
             return self.criterion(F.softmax(scores, -1), F.softmax(gold_scores, -1))
 

vizbert/run/train_lowrank_bert.py
@@ -29,12 +29,14 @@ def train(args):
         model.zero_grad()
         model2.zero_grad()
         if not trainer.training:
+            if dev_ds.multilabel:
+                scores = (scores > 0).float()
             all_scores.append(scores.detach())
             all_gold_scores.append(batch.labels.to(scores.device))
-        if batch.labels is not None:
+        if batch.labels is not None and not dev_ds.multilabel:
             accuracy = (batch.labels.to(scores.device) == scores.max(1)[1]).float().sum() / token_ids.size(0)
         else:
-            accuracy = 0
+            accuracy = torch.zeros(1).to(scores.device)
         if trainer.training:
             scheduler.step()
         return {LOSS_KEY: loss,
@@ -119,13 +121,18 @@ def train(args):
         all_scores = []
         all_gold_scores = []
         trainer.evaluate(trainer.dev_loader, 'Dev')
-        quality = train_ds.evaluate_metrics(torch.cat(all_scores), torch.cat(all_gold_scores))[args.quality_key].item()
-        print(quality)
-        if not args.eval_only:
-            torch.save(model, args.checkpoint)
-            if quality > args.orig_quality * 0.95:
-                print(f'found {rank}')
-                break
+        try:
+            qualities = train_ds.evaluate_metrics(torch.cat(all_scores), torch.cat(all_gold_scores))
+            print(qualities)
+            quality = qualities[args.quality_key].item()
+            print(quality)
+            if not args.eval_only:
+                torch.save(model, args.checkpoint)
+                if quality > args.orig_quality * 0.95:
+                    print(f'found {rank}')
+                    break
+        except:
+            pass
 
 
 def main():